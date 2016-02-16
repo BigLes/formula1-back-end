737326630d80b73cfafb1891477da27982c38c12
BigLes <bigles@bigmir.net>
2016-02-16 19:52:39 +0100

    finished main server work

---
+++ models/room.js
@@ -0,0 +1,102 @@
+'use strict';
+
+const ids = require('../common/ids');
+
+class Room {
+    constructor () {
+        this.connections = [];
+        this.notStarted = true;
+        this.roomOpen = true;
+        this.positions = require('../common/startPositions');
+        this.timeToStart = 15;
+    }
+
+    onConnect (connection) {
+
+        if (this.roomOpen) {
+            connection.player = {id: this.connections.length};
+            this.connections.push(connection);
+
+            if ((this.connections.filter((connection) => connection.connected).length >= 2) && this.notStarted) {
+                this._countDown();
+                this.notStarted = false;
+            }
+
+            this._addEvent(connection);
+        }
+    }
+
+    onClose (connection) {
+        connection.player.teamChoosed = false;
+        if (this.connections.filter((connection) => connection.connected).length === 0) {
+            this.connections = [];
+            this.notStarted = true;
+            this.roomOpen = true;
+        }
+    }
+
+    _addEvent (connection) {
+        connection.on('message', (message) => {
+            let msg = JSON.parse(message.utf8Data);
+
+            this.connections.forEach((tempConnection) => {
+                msg.data[9] = [[ids[connection.player.id][tempConnection.player.id]]];
+                if ((tempConnection !== connection) && (tempConnection.connected)) {
+                    console.log('----', JSON.stringify(msg));
+                    tempConnection.send(JSON.stringify(msg));
+                }
+            });
+
+            if (msg.data[0][0][0] === "ChangeTeam") {
+                connection.player.teamChoosed = true;
+                connection.player.name = msg.data[2][0][0];
+                const connectedCount = this.connections.filter((connection) => connection.connected).length;
+                const teamsCount = this.connections.filter((connection) => connection.connected && connection.player.teamChoosed).length;
+                this._sendToAll(["Loading", teamsCount]);
+                if (connectedCount === teamsCount) {
+                    this._sendToAll(["TeamsSelected"]);
+                    this._sendToAll(["Loading", 0]);
+                }
+                msg.data[0][0][0] = "SetOwnPosition";
+                msg.data[2][0][0] = this.positions[connection.player.id][0];
+                msg.data[3][0][0] = this.positions[connection.player.id][1];
+                connection.send(JSON.stringify(msg));
+            }
+        });
+    }
+
+    _sendToAll (message) {
+        let i = message.length;
+        let msg = {
+            c2array: true,
+            size: [10,1,1],
+            data: [[[0]],[[0]],[[0]],[[0]],[[0]],[[0]],[[0]],[[0]],[[0]],[[0]]]
+        };
+
+        while (i--) {
+            msg.data[i][0][0] = message[i];
+        }
+        if (app.settings.console) {
+            console.log(JSON.stringify(msg));
+        }
+
+        this.connections.forEach((connection) => {
+            connection.send(JSON.stringify(msg));
+        });
+    }
+
+    _countDown () {
+        let i = 0,
+            int;
+        int = setInterval(() => {
+            this._sendToAll(["Loading", i]);
+            if (i++ >= this.timeToStart) {
+                clearInterval(int);
+                this._sendToAll(["ChooseTeams", this.connections.filter((connection) => connection.connected).length]);
+                this.roomOpen = false;
+            }
+        }, 1000);
+    }
+}
+
+module.exports = Room;
---
+++ settings/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = {
+    develop: {
+        port: 8000,
+        console: true
+    },
+    production: {
+        port: 8000,
+        console: false
+    }
+};
